1dbc16d251059bb2983a4b3084d1dbac6b1cf40e
root <you@example.com>
2020-10-25 13:30:05 +0100

    Added ingredient model+endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
     path('', include('core.urls'))
 ]
 
